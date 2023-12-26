938663a25b3169a38d0af44b796efb76361a6387
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-12-25 19:40:41 -0500

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 // import { useState } from 'react'
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { About} from "./pages/About"
+import { Store} from "./pages/Store"
+import { Container } from "react-bootstrap"
 
 function App() {
   return (
-    <h1>Hii</h1>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/store" element={<Store/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Container>
   )
 }
 
